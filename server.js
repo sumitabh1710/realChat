3901a5dc19a9141c0a5901b52694057f43716eef
Sumitabh1710 <sumitabh1710@gmail.com>
2024-03-13 20:30:37 +0530

    Done

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
 
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
+
+let connectedClients = 0;
+
 app.use(express.static(__dirname + '/public'));
+
+const ioOptions = {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+};
+const io = new socketIo.Server(server, ioOptions);
 
 io.on('connection', socket => {
     console.log('A user connected');
+    connectedClients++;
+
+    io.emit('client count', connectedClients);
 
     socket.on('disconnect', () => {
         console.log('User disconnected');
+        connectedClients--;
+        io.emit('client count', connectedClients);
     });
 
     socket.on('chat message', msg => {
